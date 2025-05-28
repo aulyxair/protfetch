ff72f88260aa714fafb072166d382afaa89103da
Ingmar Lesch <ge92juk@mytum.de>
2025-05-28 07:18:23 +0200

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,16 +1,11 @@
-from pathlib import Path
-
 import pytest
+from pathlib import Path
+from protfetch.utils import parse_gene_list_file, GeneInput
 
-from protfetch.utils import GeneInput, parse_gene_list_file
-
-
-def test_parse_gene_list_file_formats(
-    sample_gene_list_file: Path,
-):  # Path is now defined
+def test_parse_gene_list_file_formats(sample_gene_list_file: Path):
     parsed_genes = parse_gene_list_file(str(sample_gene_list_file))
     assert len(parsed_genes) == 3
-
+    
     assert isinstance(parsed_genes[0], GeneInput)
     assert parsed_genes[0].gene_symbol == "PRKACA"
     assert parsed_genes[0].query_keyword == "Protein Kinase A"
